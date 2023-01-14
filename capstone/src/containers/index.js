afc2277b54d60d75a95dddaf231e0a04c7eef941
Matthew Ng <nglingshan30@gmail.com>
2023-01-14 02:11:22 -0700

    Additional Commit
    I've added some dummy data just to see if the carousel works

---
+++ capstone/src/containers/index.js
@@ -0,0 +1,12 @@
+import Header from './Header/Header';
+import Gallery from './Gallery/Gallery';
+import Benefits from './Benefits/Benefits';
+import Footer from './Footer/Footer';
+
+
+export {
+  Header,
+  Gallery,
+  Benefits,
+  Footer,
+};
