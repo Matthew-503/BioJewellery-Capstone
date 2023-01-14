b628b20c76e5ba01502d530f85c99b717a015e17
Matthew Ng <nglingshan30@gmail.com>
2023-01-14 02:09:19 -0700

    Exported Components needed

---
+++ capstone/src/components/index.js
@@ -0,0 +1,14 @@
+import SubHeading from './SubHeading/SubHeading';
+import Navbar from './Navbar/Navbar';
+
+// import Newsletter from './Footer/Newsletter';
+// import FooterOverlay from './Footer/FooterOverlay';
+// import MenuItem from './Menuitem/MenuItem';
+
+export {
+    SubHeading,
+    Navbar,
+    // Newsletter,
+    // FooterOverlay,
+    // MenuItem,
+};
