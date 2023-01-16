c841fb270bb1783568df6b3a35e3fad2f8411e4f
Matthew Ng <nglingshan30@gmail.com>
2023-01-16 09:29:29 -0700

    Comments for App

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+// Author: Ling Shan Matthew Ng
+// Version 0.1
+// Date: 15/1/2023
 
-// import { AboutUs, Footer, Gallery, Header, } from '../src/containers';
-// import { Navbar } from './components';
-// components are things that are reused in multiple containers
+// Description: This is the home page. 
+// Precondition: A home page with required containers and functional carousel
+// Postcondition: Home page with a scrollable carousel
+
+// Input: Currently no input available
+// Output: Currently no specific output
+
+import React from 'react';
 
 import './App.css';
 import { Navbar } from './components';
 import { Header, Gallery, Benefits, Footer } from './containers';
+// components are things that are reused in multiple containers
+
 
 const App = () => (
     <div>
