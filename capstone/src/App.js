564e2baaa22c6c20415317ba18106221ac0e4af2
Matthew Ng <nglingshan30@gmail.com>
2023-01-16 09:22:04 -0700

    Modified App + started comments

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+// Author: Ling Shan Matthew Ng
+// Version 0.1
+// Date: 15/1/2023
 
-// import { AboutUs, Footer, Gallery, Header, } from '../src/containers';
-// import { Navbar } from './components';
-// components are things that are reused in multiple containers
+// Description: This 
+// Precondition
+// Postcondition
+
+// Input 
+// Output
+
+
+import React from 'react';
 
 import './App.css';
 import { Navbar } from './components';
-import { Header, Gallery, Benefits, Footer, ShopCategory, Blog } from './containers';
+import { Header, Gallery, Benefits, Footer, Blog } from './containers';
+// components are things that are reused in multiple containers
 
 const App = () => (
     <div>
