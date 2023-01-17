c020704f7d39f2f29f9f8df30fd5204a18c3e58c
Matthew Ng <nglingshan30@gmail.com>
2023-01-17 12:39:43 -0700

    Updated version for app

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,13 +1,16 @@
 // Author: Ling Shan Matthew Ng
-// Version 0.1
+// Version 0.2
 // Date: 15/1/2023
 
-// Description: This 
-// Precondition
-// Postcondition
+// Description: This is the home page. 
+// Precondition: A home page with required containers and functional carousel
+// Postcondition: Home page with a scrollable carousel
 
-// Input 
-// Output
+// Input: Currently no input available
+// Output: Currently no specific output
+
+// Notes: Aim to make the header text in carousel scroll with the images
+// Notes: Will implement the images for background
 
 
 import React from 'react';
