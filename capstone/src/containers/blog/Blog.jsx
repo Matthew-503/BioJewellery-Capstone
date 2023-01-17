43aca5abc2c63fe79787ac50a8487f602923db24
Matthew Ng <nglingshan30@gmail.com>
2023-01-17 12:28:01 -0700

    Added SubHeading

--- capstone/src/containers/blog/Blog.jsx
+++ capstone/src/containers/blog/Blog.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import './Blog.css';
+
 import { Article } from '../../components';
+// import { SubHeading } from '../../components';
 import { blog02, blog03, blog04, blog05 } from './imports.js';
+
+import './Blog.css';
 
 const Blog = () => {
     return (
         <div className="gpt3__blog section__padding" id="blog">
+
+            {/* <SubHeading title="Chef's word" /> */}
+
             <div className="gpt3__blog-container">
                 <div className="gpt3__blog-container_groupB">
                     <Article
