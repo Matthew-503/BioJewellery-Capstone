7073830aec4daa87d72f454a2be3605c3c6d2bf9
Matthew Ng <nglingshan30@gmail.com>
2023-01-18 11:55:22 -0700

    Implemented SubHeading and modified Benefits

--- capstone/src/containers/Benefits/Benefits.jsx
+++ capstone/src/containers/Benefits/Benefits.jsx
@@ -1,20 +1,34 @@
+// Author: Ling Shan Matthew Ng
+// Version 0.1  
+// Date: 18/1/2023
+
+// Description: This is the benefits container. 
+// Precondition: A benefits container that contains 3 benefits 
+// Postcondition: Benefits container containing all required benefits
+
+// Input: Currently no input available
+// Output: Currently no specific output
+
+// Notes: Aim to make the subheader text have better padding / margins
+
+
 import React from 'react';
 import { BiSupport } from 'react-icons/bi';
 import { FiPackage } from 'react-icons/fi';
 import { MdPayment } from 'react-icons/md';
 
-import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Benefits.css';
 
 const Benefits = () => (
     <div className='app__benefits section__padding' id='login'>
-        <h1 className="app__benefits-headtext">
-            Benefits for your expendicy
-        </h1>
+
+        <SubHeading title="Benefits For Your Expendicy" />
+
         <div className='app__benefits-links'>
             <div className='app__benefits-links_work'>
                 <div className='app__benefits-links_icons'>
-                    <BiSupport />
+                    <MdPayment />
                 </div>
 
                 <h1 className='app__benefits-title'>
@@ -33,7 +47,7 @@ const Benefits = () => (
                 </div>
 
                 <h1 className='app__benefits-title'>
-                    Warrnety Policy
+                    Warranty Policy
                 </h1>
 
                 <p className='app__benefits-paragraph'>
@@ -44,7 +58,7 @@ const Benefits = () => (
 
             <div className='app__benefits-links_work'>
                 <div className='app__benefits-links_icons'>
-                    <MdPayment />
+                    <BiSupport />
                 </div>
 
                 <h1 className='app__benefits-title'>
