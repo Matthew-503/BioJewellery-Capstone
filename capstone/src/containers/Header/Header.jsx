6d1c918ddfd2d50814f063d7887686488a3c73b4
Matthew Ng <nglingshan30@gmail.com>
2023-01-18 11:54:28 -0700

    Added background image to Header section

--- capstone/src/containers/Header/Header.jsx
+++ capstone/src/containers/Header/Header.jsx
@@ -1,11 +1,28 @@
+// Author: Ling Shan Matthew Ng
+// Version 0.2
+// Date: 18/1/2023
+
+// Description: This is the header container
+// Precondition: A header container that has main header, descriptions, and a button
+// Postcondition: Header container with all required components
+
+// Input: Currently no input available
+// Output: Currently no specific output
+
+// Notes: The background image has been cropped to fit the styling. If any changes are needed, feel free to do the same thing
+
+
 import React from 'react';
-// import { SubHeading } from '../../components';
-// import { images } from '../../constants';
+import { images } from '../../constants';
 
 import './Header.css';
 
 const Header = () => (
-    <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__header app__bg app__wrapper section__padding overlay' id='home'>
+        <div className="app__aboutus-overlay flex__center">
+            <img src={images.headerImage} alt="G_overlay" />
+        </div>
+
         <div className='app__wrapper_info'>
             <h1 className='app__header-h1'>Discover the beauty of the BioJewerly Collection</h1>
             <p className='p__opensans' style={{ margin: '2rem 0' }}>
