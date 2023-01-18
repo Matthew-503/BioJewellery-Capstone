6225cf198afe27c4432b80f49908f026a19c8749
Matthew Ng <nglingshan30@gmail.com>
2023-01-18 11:55:41 -0700

    Updated version comments

--- capstone/src/components/index.js
+++ capstone/src/components/index.js
@@ -1,14 +1,21 @@
+// Author: Ling Shan Matthew Ng
+// Version 0.1
+// Date: 18/1/2023
+
+// Description: Index page for components 
+// Precondition: Able to import and export the components 
+// Postcondition: All functions fulfilled
+
+// Input: 4 components
+// Output: 4 components
+
+// Notes: 
+
+
 import SubHeading from './SubHeading/SubHeading';
 import Navbar from './Navbar/Navbar';
-
-// import Newsletter from './Footer/Newsletter';
-// import FooterOverlay from './Footer/FooterOverlay';
-// import MenuItem from './Menuitem/MenuItem';
 
 export {
     SubHeading,
     Navbar,
-    // Newsletter,
-    // FooterOverlay,
-    // MenuItem,
 };
--- capstone/src/constants/images.js
+++ capstone/src/constants/images.js
@@ -1,3 +1,17 @@
+// Author: Ling Shan Matthew Ng
+// Version 0.1
+// Date: 18/1/2023
+
+// Description: Index page for images 
+// Precondition: Able to import and export the images from assets 
+// Postcondition: All functions fulfilled
+
+// Input: 10 images 
+// Output: 10 images 
+
+// Notes: 
+
+
 import bg from '../assets/bg.png';
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
