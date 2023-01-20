7ae4f24750810b572a7b948354c270e70fa81008
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-01-20 10:57:35 -0700

    Review needed before merging

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 
-// import { AboutUs, Footer, Gallery, Header, } from '../src/containers';
-// import { Navbar } from './components';
+import { AboutUs, Footer, Gallery, Header, } from '../src/containers';
+import { Navbar } from './components';
 // components are things that are reused in multiple containers
 
 import './App.css';
-//import { Navbar } from './components';
-// import {Uploader} from './components';
-// import { Header, Gallery, Benefits, Footer } from './containers';
+import { Navbar } from './components';
+import { Header, Gallery, Benefits, Footer } from './containers';
 
-import {AddProduct} from './components';
+
 const App = () => (
     <div>
-        {/* <Navbar />
+        <Navbar />
         <Header />
         <Gallery />
         <Benefits />
-        <Footer /> */}
-        <AddProduct />
-        {/* <Uploader /> */}
+        <Footer />
     </div>
 );
 
