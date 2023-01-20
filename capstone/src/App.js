2b4f33dbc62633abf61b73aba092f28bb753c38f
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-01-20 12:56:01 -0700

    little changes

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 
-import { AboutUs, Footer, Gallery, Header, } from '../src/containers';
-import { Navbar } from './components';
+// import { AboutUs, Footer, Gallery, Header, Benefits} from '../src/containers';
+// import { Navbar } from './components';
+// import { Uploader } from './components';
+import {AddProduct} from './components';
 // components are things that are reused in multiple containers
 
 import './App.css';
-import { Navbar } from './components';
-import { Header, Gallery, Benefits, Footer } from './containers';
 
 
 const App = () => (
     <div>
-        <Navbar />
+        {/* <Navbar />
         <Header />
         <Gallery />
         <Benefits />
-        <Footer />
+        <Footer /> */}
+        {/* <Uploader/> */}
+        <AddProduct/>
     </div>
 );
 
--- capstone/src/components/index.js
+++ capstone/src/components/index.js
@@ -1,16 +1,17 @@
-import SubHeading from './SubHeading/SubHeading';
-import Navbar from './Navbar/Navbar';
-
-import Uploader from "./Uploader/Uploader";
-import Newsletter from './Footer/Newsletter';
-import FooterOverlay from './Footer/FooterOverlay';
-import MenuItem from './Menuitem/MenuItem';
+// import SubHeading from './SubHeading/SubHeading';
+// import Navbar from './Navbar/Navbar';
+import AddProduct from './AddProduct/AddProduct';
+// import Uploader from "./Uploader/Uploader";
+// import Newsletter from './Footer/Newsletter';
+// import FooterOverlay from './Footer/FooterOverlay';
+///import MenuItem from './Menuitem/MenuItem';
 
 export {
-    SubHeading,
-    Navbar,
-    Newsletter,
-    FooterOverlay,
-    MenuItem,
-    Uploader
+    // SubHeading,
+    // Navbar,
+    // Newsletter,
+    // FooterOverlay,
+    // MenuItem,
+    // Uploader
+    AddProduct
 };
