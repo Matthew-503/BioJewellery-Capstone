7c079f1460dc8c9735c189703d82553fa1e130a0
Matthew Ng <nglingshan30@gmail.com>
2023-01-20 10:10:44 -0700

    Added and modified assets

--- capstone/src/containers/index.js
+++ capstone/src/containers/index.js
@@ -2,16 +2,17 @@ import Header from './Header/Header';
 import Gallery from './Gallery/Gallery';
 import Benefits from './Benefits/Benefits';
 import Footer from './Footer/Footer';
-import ShopCategory from './ShopCategory/ShopCategory';
 import Blog from './blog/Blog';
 import Follow from './FollowUs/Follow';
+import Video from './Video/Video';
 
 export {
   Header,
   Gallery,
   Benefits,
   Footer,
-  ShopCategory,
   Blog,
   Follow,
+  Video,
+
 };
