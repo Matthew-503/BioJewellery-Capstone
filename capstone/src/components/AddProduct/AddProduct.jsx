15006a12026c6920064ffec7b2f75ca1072488e9
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-01-20 13:21:40 -0700

    Add Product Reviewed

--- capstone/src/components/AddProduct/AddProduct.jsx
+++ capstone/src/components/AddProduct/AddProduct.jsx
@@ -1,42 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './AddProduct.css';
 
-const AddProduct = () => {
-    
-    return(
-        <AddProduct className="flex__center">
-            <div>
-                <form>
-                <h1>Add Product</h1>
-                <p>Product Information</p>
-                    <label>
-                        Product Name
-                        <input name="name" type="text" placeholder="Product Name"/>
-                    </label>
-                    
-                    <label>
-                        Product Description 
-                        <input name="descrption" type="text" placeholder="Description"/>
-                    </label>
-                     
-                    <label>
-                        Price 
-                        <input name="price" type="number" placeholder="Price per unit"/>
-                    </label>
-                     
-                    <label>
-                        In-Stock
-                        <input name="stock" type="number" placeholder="Number of Products in Stock"/>
-                    </label>
-                    
-                    <div>
-                        <button className='custom__button'>Save</button>
-                        <button className='custom__button'>Cancel</button>
-                    </div>                    
-                </form>
-            </div>            
-        </AddProduct>
+class AddProduct extends Component {
+   
+    render() 
+    { 
+        return(
+            <div className='flex__center'>
+            <form>
+            <h1>Add Product</h1>
+            <p>Product Information</p>
+                <label>
+                    Product Name<br/>
+                    <input name="name" type="text" placeholder="Product Name"/><br/>
+                </label>
+                
+                <label>
+                    Product Description<br/> 
+                    <input name="descrption" type="text" placeholder="Description"/><br/>
+                </label>
+                
+                <label>
+                    Price <br/>
+                    <input name="price" type="number" placeholder="Price per unit"/><br/>
+                </label>
+                
+                <label>
+                    In-Stock<br/>
+                    <input name="stock" type="number" placeholder="Number of Products in Stock"/><br/>
+                </label>
+                
+                <div>
+                    <button className='custom__button' >Save</button>
+                    <button className='custom__button'>Cancel</button>  
+                </div>             
+            </form>     
+            </div>
     );
-};
+    }
+}
 
 export default AddProduct;
