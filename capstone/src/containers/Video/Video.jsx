81d1a9d2520e0a9abef3123eeec4a59245c9314c
Matthew Ng <nglingshan30@gmail.com>
2023-01-26 15:22:50 -0700

    Modified Video

--- capstone/src/containers/Video/Video.jsx
+++ capstone/src/containers/Video/Video.jsx
@@ -1,45 +1,45 @@
-import React from 'react';
-import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
-import { video } from '../../constants';
+// import React from 'react';
+// import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+// import { video } from '../../constants';
 
-import './Video.css';
+// import './Video.css';
 
-const Intro = () => {
-    const [playVideo, setPlayVideo] = React.useState(false);
-    const vidRef = React.useRef();
+// const Intro = () => {
+//     const [playVideo, setPlayVideo] = React.useState(false);
+//     const vidRef = React.useRef();
 
-    return (
-        <div className='video'>
-            <video
-                ref={vidRef}
-                src={video}
-                type='video/mp4'
-                loop
-                controls={false}
-                muted
-            />
+//     return (
+//         <div className='video'>
+//             <video
+//                 ref={vidRef}
+//                 src={video}
+//                 type='video/mp4'
+//                 loop
+//                 controls={false}
+//                 muted
+//             />
 
-            <div className='video__overlay app__flex-center'>
-                <div
-                    className='video__overlay-circle app__flex-center'
-                    onClick={() => {
-                        setPlayVideo(!playVideo);
-                        if (playVideo) {
-                            vidRef.current.pause();
-                        }
-                        else {
-                            vidRef.current.play();
-                        }
-                    }}
-                >
-                    {playVideo
-                        ? <BsPauseFill color='#fff' fontSize={30} />
-                        : <BsFillPlayFill color='#fff' fontSize={30} />
-                    }
-                </div>
-            </div>
-        </div>
-    );
-}
+//             <div className='video__overlay app__flex-center'>
+//                 <div
+//                     className='video__overlay-circle app__flex-center'
+//                     onClick={() => {
+//                         setPlayVideo(!playVideo);
+//                         if (playVideo) {
+//                             vidRef.current.pause();
+//                         }
+//                         else {
+//                             vidRef.current.play();
+//                         }
+//                     }}
+//                 >
+//                     {playVideo
+//                         ? <BsPauseFill color='#fff' fontSize={30} />
+//                         : <BsFillPlayFill color='#fff' fontSize={30} />
+//                     }
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 
-export default Intro;
+// export default Intro;
