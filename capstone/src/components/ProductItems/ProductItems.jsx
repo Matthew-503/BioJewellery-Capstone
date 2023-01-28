45c90656cd3ceead9a31237e182ebbf9008984cc
NaomyTung <106546446+NaomyTung@users.noreply.github.com>
2023-01-28 00:06:00 -0700

    Changes in ShopProduct, and ProductItems
    Added the routing for those pages. Added link var in the productitems to get the link based on the id of the product in shopproduct

--- capstone/src/components/ProductItems/ProductItems.jsx
+++ capstone/src/components/ProductItems/ProductItems.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './ProductItems.css';
 
-const ProductItems = ({ imgUrl, price, text }) => {
+const ProductItems = ({ imgUrl, price, text, url }) => {
     return (
         <div className="gpt3__blog-container_article">
             <div className="gpt3__blog-container_article-image">
-                <img src={imgUrl} alt="blog" />
+                <Link  to={url}>
+                    <img src={imgUrl} alt="blog" />
+                </Link>
+               
             </div>
 
             <div className="gpt3__blog-container_article-content">
                 <div>
-                    <h3>{text}</h3>
+                    <Link  to={url}>
+                        <h3>{text}</h3>
+                    </Link>
                     <h4>${price}</h4>
                 </div>
             </div>
