4e2e60ee7b7cdf45cb8c74285ef9108add2451cd
Matthew Ng <nglingshan30@gmail.com>
2023-02-22 01:12:40 -0700

    Comment back needed stuff

--- capstone/src/containers/Home/Home.jsx
+++ capstone/src/containers/Home/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <div>
 
-      {/* <Header /> */}
-      {/* <Gallery /> */}
+      <Header />
+      <Gallery />
       {/* <Video /> */}
       <Benefits />
       <Follow />
