7394d16f283b3734d2b57caff04c85c36a83576f
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-02-22 20:25:48 -0700

    Needed code Review
    When I tested the route getReview, it is throwing up authorization error but I haven't added the authMiddleware to this route. Can anyone help with this issue?

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { protect } = require('../middleware/authMiddleware')
 const { getReview, createReview, updateReview, deleteReview } = require('../controllers/reviewController');
 
+//error for authorization when testing getReview route. Help!
 router.route('/').get(getReview).post(protect, createReview);
 
 router.route('/:reviewId').put(protect, updateReview).delete(protect, deleteReview);
