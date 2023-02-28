68f23eafd7c280ede2b162186721f828d5da5089
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-02-28 16:32:20 -0700

    updated cartModel
    added a virtual field for subTotal

--- backend/models/cartModel.js
+++ backend/models/cartModel.js
@@ -21,8 +21,12 @@ const cartSchema = new mongoose.Schema({
     timestamps: true
 });
 
-cartSchema.virtual('subtotal').get(function(){
-    return this.f
+cartSchema.virtual('subTotal').get(function(){
+    let sum = 0
+    this.products.forEach((product) => {
+        sum += (product.productId.finalPrice * product.quantity)
+    })
+    return sum
 })
 
 module.exports = mongoose.model('Cart', cartSchema);
