5b304627a550b43f9a6393a7a9c743de4981a7dd
Matthew Ng <nglingshan30@gmail.com>
2023-03-03 21:41:54 -0700

    Shop Category will be removed as per meeting

--- capstone/src/constants/categories.js
+++ capstone/src/constants/categories.js
@@ -2,10 +2,13 @@ import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import ColorLensIcon from '@mui/icons-material/ColorLens';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import TrendingUp from '@mui/icons-material/TrendingUp'
+import StarIcon from '@mui/icons-material/Star';
 
 export default [
-    { name: 'Price', icon: <AttachMoneyIcon />, },
-    { name: 'Color', icon: <ColorLensIcon />, },
+    { name: 'Popular', icon: <StarIcon />, },
+    { name: 'Trending', icon: <TrendingUp />, },
+    { name: 'On Sale', icon: <AttachMoneyIcon />, },
     { name: 'Ascd', icon: <ArrowUpwardIcon />, },
     { name: 'Dscd', icon: <ArrowDownwardIcon />, },
 ];
