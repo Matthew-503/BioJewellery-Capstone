b178bae5c867b247ebbd09adafc5f50e892da8bf
Matthew Ng <nglingshan30@gmail.com>
2023-03-09 00:17:49 -0700

    Update Home.jsx

--- capstone/src/containers/Home/Home.jsx
+++ capstone/src/containers/Home/Home.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import { Header, Gallery, Follow, Benefits, ProductDetail, Login, SignUp, AboutUs, Error, Policy, Feedback } from '../../containers';
+import { Header, Gallery, Follow, Benefits, ProductDetail, Login, SignUp, AboutUs, Error, Policy, Feedback, Account } from '../../containers';
 const Home = () => {
   return (
     <div>
 
-      <Header />
-      <Gallery />
+      {/* <Header /> */}
+      {/* <Gallery /> */}
       {/* <Video /> */}
-      <Benefits />
-      <Follow />
+      {/* <Benefits /> */}
+      {/* <Follow /> */}
       {/* <Login /> */}
       {/* <SignUp /> */}
       {/* <AboutUs /> */}
       {/* <Error /> */}
       {/* <Policy /> */}
       {/* <Feedback /> */}
+      <Account />
     </div>
   )
 }
