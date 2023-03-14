7b370276f238379e902fdac88d466831c7dcb584
Matthew Ng <nglingshan30@gmail.com>
2023-03-14 13:02:33 -0600

    Updated shipping

--- capstone/src/containers/Shipping/Shipping.jsx
+++ capstone/src/containers/Shipping/Shipping.jsx
@@ -15,75 +15,167 @@ import { SubHeading } from '../../components';
 
 import './Shipping.css';
 
+const Category1 = 'Shipping Details';
+const Category2 = 'Payment Method';
+const Category3 = 'Product Details';
+
+const FirstName = 'First Name';
+const LastName = 'Last Name';
+const Phone = '123 456 789';
+const Street = '17 Ave SW';
+const City = 'Calgary';
+const Postal = 'A3B 0DE';
+const Province = 'AB';
+const Country = 'Canada';
+
+const Card = '1234 5678 9101';
+const CardName = 'Name';
+const CVV = '001';
+const Expiry = '01/01/2024';
+
+const Product = 'Golden Leaf Earing';
+const Qty = '100';
+const TotalPrice = 100;
+const Warranty = 'Available 1 year from purchase';
+
 const Shipping = () => {
     return (
-        <div className='confirm app__section-padding'>
-            <div className='confirm__headtext'>
-                <SubHeading title={"Thank you for ordering"} />
+        <div className='order app__section-padding'>
+            <SubHeading title={'Thank you for ordering'} />
+            <div className='order__pt1'>
+                <div className='order__category'>
+                    <p className='order__detail-category'>
+                        {Category1}
+                    </p>
+                </div>
 
-                <br></br>
-                <table className='confirm__shipping-table'>
-                    <tr>
-                        <th rowSpan={6}>Shipping Details</th>
-                    </tr>
-                    <tr>
-                        <td>Bob</td>
-                        <td>Lee</td>
-                    </tr>
-                    <tr>
-                        <td colSpan={2}>123 456 789</td>
-                    </tr>
-                    <tr>
-                        <td colSpan={2}>17 Ave SW</td>
-                    </tr>
-                    <tr>
-                        <td>Calgary</td>
-                        <td>T3J 0H8</td>
-                    </tr>
-                    <tr>
-                        <td>AB</td>
-                        <td>Canada</td>
-                    </tr>
-                </table>
+                <div className='order__detail-long'>
+                    <div className='order__detail-short'>
+                        <div>
+                            <p className='order__detail'>
+                                {FirstName}
+                            </p>
+                        </div>
 
-                <table className='confirm__payment-table'>
-                    <tr>
-                        <th rowSpan={5}>Payment Details</th>
-                    </tr>
-                    <tr>
-                        <td>123 4567 8910</td>
-                    </tr>
-                    <tr>
-                        <td>Bob Lee</td>
-                    </tr>
-                    <tr>
-                        <td>012</td>
-                    </tr>
-                    <tr>
-                        <td>01/29</td>
-                    </tr>
-                </table>
+                        <div>
+                            <p className='order__detail'>
+                                {LastName}
+                            </p>
+                        </div>
+                    </div>
 
-                <table className='confirm__product-table'>
-                    <tr>
-                        <th rowSpan={5}>Product Details</th>
-                    </tr>
-                    <tr>
-                        <td>Gold leaf necklace</td>
-                        <td rowSpan={5}>
-                            <img src={productpic} alt="product_image" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>1 qty</td>
-                    </tr>
-                    <tr>
-                        <td>$1000.00</td>
-                    </tr>
-                    <tr>
-                        <td>Warranty: 1 year from purchase</td>
-                    </tr>
-                </table>
+                    <div>
+                        <div>
+                            <p className='order__detail'>
+                                {Phone}
+                            </p>
+                        </div>
+
+                        <div>
+                            <p className='order__detail'>
+                                {Street}
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className='order__detail-long'>
+                        <div className='order__detail-short'>
+                            <div>
+                                <p className='order__detail'>
+                                    {City}
+                                </p>
+                            </div>
+
+                            <div>
+                                <p className='order__detail'>
+                                    {Postal}
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className='order__detail-short'>
+                            <div>
+                                <p className='order__detail'>
+                                    {Province}
+                                </p>
+                            </div>
+
+                            <div>
+                                <p className='order__detail'>
+                                    {Country}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='order__pt2'>
+                <div className='order__category'>
+                    <p className='order__detail-category'>
+                        {Category2}
+                    </p>
+                </div>
+
+                <div className='order__detail-long'>
+                    <div>
+                        <p className='order__detail'>
+                            Card Number: {Card}
+                        </p>
+                    </div>
+
+                    <div>
+                        <p className='order__detail'>
+                            Card Name: {CardName}
+                        </p>
+                    </div>
+
+                    <div>
+                        <p className='order__detail'>
+                            CVV: {CVV}
+                        </p>
+                    </div>
+
+                    <div>
+                        <p className='order__detail'>
+                            Expiry Date: {Expiry}
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='order__pt3'>
+                <div className='order__category'>
+                    <p className='order__detail-category'>
+                        {Category3}
+                    </p>
+                </div>
+
+                <div className='order__detail-long'>
+                    <div>
+                        <p className='order__detail'>
+                            Product Name: {Product}
+                        </p>
+                    </div>
+
+                    <div>
+                        <p className='order__detail'>
+                            Quantity: {Qty}
+                        </p>
+                    </div>
+
+                    <div>
+                        <p className='order__detail'>
+                            Total Price: ${TotalPrice}
+                        </p>
+                    </div>
+
+                    <div>
+                        <p className='order__detail'>
+                            Warranty: {Warranty}
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     );
