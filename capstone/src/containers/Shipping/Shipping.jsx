d5a50c9029ab22c5b7b08f4e7da6df55bce6a783
Matthew Ng <nglingshan30@gmail.com>
2023-03-14 22:19:15 -0600

    Added shipping page and have mobile view

--- capstone/src/containers/Shipping/Shipping.jsx
+++ capstone/src/containers/Shipping/Shipping.jsx
@@ -9,9 +9,10 @@
 *
  */
 
-import React from 'react'
+import React, { useState } from 'react';
 import productpic from '../../assets/gallery01.png'
 import { SubHeading } from '../../components';
+import { FaShoppingCart } from 'react-icons/fa';
 
 import './Shipping.css';
 
@@ -39,9 +40,41 @@ const TotalPrice = 100;
 const Warranty = 'Available 1 year from purchase';
 
 const Shipping = () => {
+
+    const [isDisabled, setIsDisabled] = useState(true);
+
+    const toggleDisabled = () => {
+        setIsDisabled(!isDisabled);
+    }
+
+    const [formData, setFormData] = useState({
+        firstname: 'YA',
+        lastname: 'BOI',
+        phone: '123 456 7890',
+        street: 'yaboi Ave SW',
+        city: 'Calgary',
+        postal: 'A3B 0DE',
+        province: 'AB',
+        country: 'Canada',
+
+        product: 'Golden Leaf Earring',
+        qty: 100,
+        totalprice: 100,
+        warranty: 'Available 1 year from purchase',
+    })
+
+    const { firstname, lastname, phone, street, city, postal, province, country, product, qty, totalprice, warranty } = formData
+
+    const changeHandler = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
     return (
         <div className='order app__section-padding'>
-            <SubHeading title={'Thank you for ordering'} />
+            <SubHeading title={'Order Details'} />
             <div className='order__pt1'>
                 <div className='order__category'>
                     <p className='order__detail-category'>
@@ -52,61 +85,108 @@ const Shipping = () => {
                 <div className='order__detail-long'>
                     <div className='order__detail-short'>
                         <div>
-                            <p className='order__detail'>
-                                {FirstName}
-                            </p>
+                            <input
+                                type="text"
+                                id="firstname"
+                                name="firstname"
+                                value={firstname}
+                                disabled={isDisabled}
+                                onChange={changeHandler}
+                            />
                         </div>
 
                         <div>
-                            <p className='order__detail'>
-                                {LastName}
-                            </p>
+                            <input
+                                type="text"
+                                id="lastname"
+                                name="lastname"
+                                value={lastname}
+                                disabled={isDisabled}
+                                onChange={changeHandler}
+                            />
                         </div>
                     </div>
 
                     <div>
                         <div>
-                            <p className='order__detail'>
-                                {Phone}
-                            </p>
+                            <input
+                                type="text"
+                                id="phone"
+                                name="phone"
+                                value={phone}
+                                disabled={isDisabled}
+                                onChange={changeHandler}
+                            />
                         </div>
 
                         <div>
-                            <p className='order__detail'>
-                                {Street}
-                            </p>
+                            <input
+                                type="text"
+                                id="street"
+                                name="street"
+                                value={street}
+                                disabled={isDisabled}
+                                onChange={changeHandler}
+                            />
                         </div>
                     </div>
 
-                    <div className='order__detail-long'>
-                        <div className='order__detail-short'>
-                            <div>
-                                <p className='order__detail'>
-                                    {City}
-                                </p>
-                            </div>
-
-                            <div>
-                                <p className='order__detail'>
-                                    {Postal}
-                                </p>
-                            </div>
+                    <div className='order__detail-short'>
+                        <div>
+                            <input
+                                type="text"
+                                id="city"
+                                name="city"
+                                value={city}
+                                disabled={isDisabled}
+                                onChange={changeHandler}
+                            />
                         </div>
 
-                        <div className='order__detail-short'>
-                            <div>
-                                <p className='order__detail'>
-                                    {Province}
-                                </p>
-                            </div>
-
-                            <div>
-                                <p className='order__detail'>
-                                    {Country}
-                                </p>
-                            </div>
+                        <div>
+                            <input
+                                type="text"
+                                id="postal"
+                                name="postal"
+                                value={postal}
+                                disabled={isDisabled}
+                                onChange={changeHandler}
+                            />
                         </div>
                     </div>
+
+                    <div className='order__detail-short'>
+                        <div>
+                            <input
+                                type="text"
+                                id="province"
+                                name="province"
+                                value={province}
+                                disabled={isDisabled}
+                                onChange={changeHandler}
+                            />
+                        </div>
+
+                        <div>
+                            <input
+                                type="text"
+                                id="country"
+                                name="country"
+                                value={country}
+                                disabled={isDisabled}
+                                onChange={changeHandler}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className='order__button'>
+                    <button
+                        className='order__button-action'
+                        onClick={toggleDisabled}>
+                        {isDisabled ? 'Edit' : 'Confirm'}
+                    </button>
                 </div>
             </div>
 
@@ -120,62 +200,37 @@ const Shipping = () => {
                 <div className='order__detail-long'>
                     <div>
                         <p className='order__detail'>
-                            Card Number: {Card}
+                            Product Name: {product}
                         </p>
                     </div>
 
                     <div>
                         <p className='order__detail'>
-                            Card Name: {CardName}
+                            Quantity: {qty}
                         </p>
                     </div>
 
                     <div>
                         <p className='order__detail'>
-                            CVV: {CVV}
+                            Total Price: ${totalprice}
                         </p>
                     </div>
 
                     <div>
                         <p className='order__detail'>
-                            Expiry Date: {Expiry}
+                            Warranty: {warranty}
                         </p>
                     </div>
                 </div>
             </div>
 
-            <div className='order__pt3'>
-                <div className='order__category'>
-                    <p className='order__detail-category'>
-                        {Category3}
-                    </p>
-                </div>
-
-                <div className='order__detail-long'>
-                    <div>
-                        <p className='order__detail'>
-                            Product Name: {Product}
-                        </p>
-                    </div>
-
-                    <div>
-                        <p className='order__detail'>
-                            Quantity: {Qty}
-                        </p>
-                    </div>
-
-                    <div>
-                        <p className='order__detail'>
-                            Total Price: ${TotalPrice}
-                        </p>
-                    </div>
-
-                    <div>
-                        <p className='order__detail'>
-                            Warranty: {Warranty}
-                        </p>
-                    </div>
-                </div>
+            <div className='order__button'>
+                <button
+                    className='order__button-submit'
+                    type='submit'
+                >
+                    Proceed to Checkout <FaShoppingCart className='order__button-icon' />
+                </button>
             </div>
         </div>
     );
