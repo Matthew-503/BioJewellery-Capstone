91183ad792c94e3b8aa0db3db6784730011e140b
Matthew Ng <nglingshan30@gmail.com>
2023-03-18 23:44:01 -0600

    Added search bar animation

--- capstone/src/components/Navbar/Navbar.jsx
+++ capstone/src/components/Navbar/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
             </ul>
 
             <div className="navbar-input">
-                <input className='navbar-input-search' placeholder='Search BioJewellery' />
+                <input className='navbar-input-search' type='text' placeholder='Search BioJewellery' />
 
                 <a href="#login">
                     <MdSearch className="navbar-icons" />
