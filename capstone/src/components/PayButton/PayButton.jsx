68ddb62a63c026c468535f258de26becd5ccde6d
Matthew Ng <nglingshan30@gmail.com>
2023-03-20 10:41:11 -0600

    Modified pay button

--- capstone/src/components/PayButton/PayButton.jsx
+++ capstone/src/components/PayButton/PayButton.jsx
@@ -11,38 +11,47 @@ import { useSelector } from "react-redux";
 
 const PayButton = () => {
 
-    // const {cartProducts} = useSelector((state) => state.cart);
+  // const {cartProducts} = useSelector((state) => state.cart);
 
-    const [cartItems, setCartItems] = useState([
-            {
-                product:{priceApiId: 'price_1MmsiHGqyagVRxA1Oce48my1'},
-                quantity: 1
-            }
-    ]);
+  const [cartItems, setCartItems] = useState([
+    {
+      product: { priceApiId: 'price_1MmsiHGqyagVRxA1Oce48my1' },
+      quantity: 1
+    }
+  ]);
 
-    const checkout = async () => {
-        // a fetch request to backend
-        await fetch('http://localhost:8001/checkout', {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({cartItems})
-        }).then((response) => {
-          //converts response to JSON format and returns
-          return response.json();
-        }).then((response) => {
-          if(response.url){
-            window.location.assign(response.url);
-          }
-        })
+  const checkout = async () => {
+    // a fetch request to backend
+    await fetch('http://localhost:8001/checkout', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ cartItems })
+    }).then((response) => {
+      //converts response to JSON format and returns
+      return response.json();
+    }).then((response) => {
+      if (response.url) {
+        window.location.assign(response.url);
       }
-    
-    return (
-        <>
-            <button className="cartbar__button" onClick={() => checkout()}> <FaShoppingCart />Proceed to Checkout</button>
-        </>
-    )
+    })
+  }
+
+  return (
+    // <>
+    //   <button className="cartbar__button" onClick={() => checkout()}>
+    //     <FaShoppingCart />
+    //     Proceed to Checkout
+    //   </button>
+    // </>
+    <div className="cartbar__button" onClick={() => checkout()} >
+      <button>
+        <FaShoppingCart className='cartbar__icon'/>
+        Proceed to Checkout
+      </button>
+    </div>
+  )
 
 }
 
