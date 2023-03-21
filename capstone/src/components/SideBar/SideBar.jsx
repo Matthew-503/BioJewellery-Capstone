e8a64a93f41480a7fc3f004330c121331d249052
Matthew Ng <nglingshan30@gmail.com>
2023-03-21 15:25:38 -0600

    Modified Sidebar

--- capstone/src/components/SideBar/SideBar.jsx
+++ capstone/src/components/SideBar/SideBar.jsx
@@ -20,13 +20,13 @@ const SideBar = ({ selectedCategory, setSelectedCategory }) => (
                     <button
                         className="category-btn grid-item"
                         onClick={() => setSelectedCategory(category.name)}
-                        style={{
-                            background: category.name === selectedCategory && "#bcf6b1",
-                            color: "#000000",
-                        }}
+                        // style={{
+                        //     background: category.name === selectedCategory && "#bcf6b1",
+                        //     color: "#000000",
+                        // }}
                         key={category.name}
                     >
-                        <span style={{ color: category.name === selectedCategory ? "black" : "#072d2e" }} className='grid-item'>
+                        <span className='grid-item'>
                             {category.icon}
                         </span>
                         <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }} className='grid-item sidebar__content'>
