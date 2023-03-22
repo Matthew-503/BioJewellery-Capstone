2a5dfd0eae50b9cf488b3cdddc3eeb490d1910dc
Matthew Ng <nglingshan30@gmail.com>
2023-03-22 13:38:41 -0600

    Update SwitchProductDetail.jsx

--- capstone/src/components/SwitchProductDetail/SwitchProductDetail.jsx
+++ capstone/src/components/SwitchProductDetail/SwitchProductDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { animated, useSpring } from "react-spring";
-import { Rating, ReviewBlock, FeatureBlock } from '..';
+import { Rating, ReviewBlock, DescriptionBlock } from '..';
 
 import './SwitchProductDetail.css';
 
@@ -16,12 +16,12 @@ const SwitchProductDetail = () => {
     //Default Variable for review block
     var customerDefaultName = "Very Cool Name";
     var customerDefaultTitle = "Default Title";
-    var customerDefaultDescription = "Default Description";
+    var customerDefaultDescription = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
-    
+
     return (
         <>
-             <div className="switch">
+            <div className="switch">
                 <br />
                 <div
                     className="switch_tab"
@@ -44,7 +44,7 @@ const SwitchProductDetail = () => {
             <div className="col text-center">
                 <p>
                     {myProfession === "Description" && (
-                        <FeatureBlock
+                        <DescriptionBlock
                             productFeatureName="Name"
                             productFeatureType="Type"
                             productFeatureColor="Type"
@@ -62,7 +62,7 @@ const SwitchProductDetail = () => {
                     )}
 
                 </p>
-            </div> 
+            </div>
         </>
     );
 };
