d608439d8f9f637a967eaff2d1a568e01827d6b0
Matthew Ng <nglingshan30@gmail.com>
2023-03-22 16:44:13 -0600

    Update App.js

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,4 +1,4 @@
-    // Author: Ling Shan Matthew Ng, Naomy Tung
+// Author: Ling Shan Matthew Ng, Naomy Tung
 // Version 1.o
 // Date: 25/1/2023
 
@@ -16,9 +16,7 @@
 import React from 'react';
 import { Link, Route, Routes } from "react-router-dom"
 import { Navbar, AddProduct, EditProduct, EmployeeMenu, Uploader } from './components';
-import { Home, Header, Gallery, Benefits, Footer, Follow, ShopProduct, ProductDetail, OrderConfirmation, ShoppingCart, UserNavigation, ShopCategory, Feed, Login } from './containers';
-
-// components are things that are reused in multiple containers
+import { Home, Header, Gallery, EmployeeReturn, Benefits, Footer, Follow, ShopProduct, ProductDetail, OrderConfirmation, ShoppingCart, UserNavigation, ShopCategory, Feed, Login, EmployeeSettings, AboutUs } from './containers';
 
 import './App.css';
 import PaymentCancellation from './containers/PaymentCancellation/PaymentCancellation';
@@ -26,58 +24,53 @@ import OrderPreview from './containers/OrderPreview/OrderPreview';
 
 //testing address component - need to be removed
 import Address from './components/Address/Address';
+import ProtectedRoute from './features/ProtectedRoute';
 
 const App = () => (
     <div>
 
-        <EmployeeSettings />
-        
-        {/*
         <Routes>
             <Route path="/" element={<UserNavigation />}>
+                <Route path="/aboutus" element={<AboutUs />} />
                 <Route index element={<Home />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/categories">
-                    <Route index element={<ShopCategory />} />
+                    <Route index element={<Feed />} />
                     <Route path=":cat" element={<Feed />} />
                 </Route>
                 <Route path="/products">
                     <Route index element={<ShopProduct />} />
                     <Route path=":name" element={<ProductDetail />} />
                 </Route>
-
                 <Route path="/cart" element={<ShoppingCart />} />
                 <Route path="/success" element={<OrderConfirmation />} />
                 <Route path="/cancel" element={<PaymentCancellation />} />
-                <Route path="/orderpreview" element={<OrderPreview />} />
+                {/* <Route path="/orderpreview" element={<OrderPreview />} /> */}
                 <Route path="/addresses" element={<Address />} />
             </Route>
-            <Route path="/add" element={<AddProduct />} />
-            <Route path="/editproduct" element={<EditProduct />} />
+            <Route element={<ProtectedRoute />}>
+                <Route path="/add" element={<AddProduct />} />
+                <Route path="/editproduct" element={<EditProduct />} />
+            </Route>
+
             <Route path="/login" element={<Login />} />
             {/* <Route path="/admin">
                 
                  <Route index element={<HomeAdmin />} />              
             </Route> */}
-
-            {/* Route specifications for the Login Page
+        </Routes>
+        {/* Route specifications for the Login Page
                 <Route path="/login" element={<LoginTemplate />}>
                 <Route index element={<Blog />} /> 
                 <Route path=":cat" element={<ShopProduct />} />  
                 //dont forget to add <Outlet context(){varname: "whatever"}/> in the Login Template     
                 //in the other page to use the context const obj = useOutletContext();   
                 //use the replace in the Link so it will go back 2 pages   
-            </Route> */}
+            </Route>  */}
 
-            {/*Route for the not found page)
-             <Route path="*" element={<NotFound />}/> 
-            */}
-        {/* </Routes> */}
-
-        {/* The uploader is causing the spacing issues, have to comment out everything inside css
-            Side note: Avoid using a <main> tag.*/}
-        {/* <Navbar />
-        <Uploader />  */}
+        {/* <Route path="*" element={<NotFound />}
+        <Navbar />
+        <Uploader />   */}
 
 
         {/* The addproduct did not cause spacing issues, but is affecting other divs, have to comment out this component */}
