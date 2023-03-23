232f5ebf470b60998cd490042f6020e6c92d8efd
Matthew Ng <nglingshan30@gmail.com>
2023-03-23 11:36:23 -0600

    Minor change

--- capstone/src/containers/Home/Home.jsx
+++ capstone/src/containers/Home/Home.jsx
@@ -4,11 +4,11 @@ const Home = () => {
   return (
     <div>
 
-      <Header />
-      <Gallery />
+      {/* <Header /> */}
+      {/* <Gallery /> */}
       {/* <Video /> */}
-      <Benefits />
-      <Follow />
+      {/* <Benefits /> */}
+      {/* <Follow /> */}
       {/* <Login /> */}
       {/* <SignUp /> */}
       {/* <AboutUs /> */}
