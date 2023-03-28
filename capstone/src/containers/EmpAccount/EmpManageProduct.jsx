77d5a75c16d5fe922e620323d2ffae44aab8ffb6
Matthew Ng <nglingshan30@gmail.com>
2023-03-28 00:24:53 -0600

    Modified emp manage product

---
+++ capstone/src/containers/EmpAccount/EmpManageProduct.jsx
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+
+import EmployeeMenu from '../../components/EmployeeMenu/EmployeeMenu';
+import Uploader from '../../components/Uploader/Uploader';
+
+// Author: Buola Achor
+// Version 0.1
+// Date: 18/1/2023
+
+// Description: This is the add product employee page. 
+// Precondition: Must be connected and be able to add to the database also have the uploader component
+// Postcondition: nothing
+
+// Input: User Product Specifications, 
+// Output: Page
+
+// class AddProduct extends Component {
+
+//     render() {
+//         return (
+//             <div className="flex__center">
+
+//                 <form>
+
+//                     <div className="add__employee-menu">
+//                         <EmployeeMenu />
+//                     </div>
+
+//                     <div className="add__name">
+//                         <h1 className="add__title">Add Product</h1>
+
+//                         <div className="add__product-info">
+//                             <p>Product Information</p>
+//                         </div>
+//                     </div>
+
+//                     <div className="add__product-uploader">
+//                         <Uploader />
+//                     </div>
+
+//                     <div className="add__product-name">
+//                         <label>
+//                             <h3 className="input__name">Product Name</h3>
+//                             <input name="name" type="text" className="add_input-name" placeholder="Product Name" />
+//                         </label>
+//                     </div>
+
+//                     <div className="add__product-description">
+//                         <label>
+//                             <h3 className="input__name">Product Description</h3>
+//                             <input name="descrption" type="text" className="add_input-desc" placeholder="Description" /><br />
+//                         </label>
+//                     </div>
+
+//                     <ul className="add__price-prod">
+//                         <li className="add__product-price">
+//                             <label>
+//                                 <h3 className="input__name">Price </h3>
+//                                 <input name="price" type="number" className="add_input-price" placeholder="Price per unit" /><br />
+//                             </label>
+//                         </li>
+
+//                         <li className="add__product-stock">
+//                             <label>
+//                                 <h3 className="input__name">In-Stock</h3>
+//                                 <input name="stock" type="number" className="add_input-stock" placeholder="Number of Products in Stock" /><br />
+//                             </label>
+//                         </li>
+//                     </ul>
+
+//                     <div className="add__buttons">
+//                         <button className="button-save" >Save</button>
+//                         <button className="button-cancel">Cancel</button>
+//                     </div>
+//                 </form>
+
+//             </div>
+//         );
+//     }
+// }
+
+import { useEffect, useState } from "react";
+import { images } from '../../constants';
+import { SubHeading, ProductDetailBar, Navbar, EmpProductItem } from '../../components';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+
+import { fetchFromAPI } from '../../constants';
+import { Box, Stack, Typography } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+
+import './EmpManageProduct.css';
+import EmpSidebarAccount from "./EmpSidebarAccount";
+
+
+const EmpManageProduct = (props) => {
+
+    const [selectedCategory, setSelectedCategory] = useState("Products");
+    const [setVideos] = useState(null);
+
+    useEffect(() => {
+        // setVideos(null);
+
+        fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+            .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
+
+    const { checked, onChange } = props;
+
+    return (
+        <div>
+            <Navbar />
+
+            <Stack sx={{ flexDirection: { sx: "column", md: "row" }, background: "var(--color-lightgreen)" }}>
+                <Box p={2} sx={{ overflowY: "auto", height: "90vh" }}>
+                    <EmpSidebarAccount selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+                </Box>
+
+                <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+                    <div className='emp__manage'>
+                        <h1 className='emp__manage-header'>
+                            Manange Products
+                        </h1>
+
+                        <div className='emp__manage-add'>
+                            <AddIcon className='emp__manage-icon '/>
+                        </div>
+
+                        <div className='emp__manage-search'>
+                            <input className='navbar-input-search' type='text' placeholder='Search BioJewellery' />
+                        </div>
+
+                        <div className='emp__manage-product'>
+                            <EmpProductItem />
+                        </div>
+                    </div>
+                </Box>
+            </Stack>
+        </div>
+    )
+}
+export default EmpManageProduct;
