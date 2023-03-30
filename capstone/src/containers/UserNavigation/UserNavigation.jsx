67f5b095e0b19a24db43716c6dc3f343f046d1b6
Matthew Ng <nglingshan30@gmail.com>
2023-03-29 23:53:44 -0600

    Update UserNavigation.jsx

--- capstone/src/containers/UserNavigation/UserNavigation.jsx
+++ capstone/src/containers/UserNavigation/UserNavigation.jsx
@@ -11,9 +11,9 @@ import { Navbar } from '../../components';
 const UserNavigation = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
