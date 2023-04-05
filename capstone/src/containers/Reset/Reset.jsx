bc92e7569e972aa9d08d9f79917d0ddcc77a67e1
Matthew Ng <nglingshan30@gmail.com>
2023-04-04 21:16:56 -0600

    Modified the reset page

--- capstone/src/containers/Reset/Reset.jsx
+++ capstone/src/containers/Reset/Reset.jsx
@@ -1,20 +1,35 @@
-import React, { useEffect, useState } from "react";
-import { images } from '../../constants';
-import { SubHeading, ProductDetailBar } from '../../components';
-import ModeEditIcon from '@mui/icons-material/ModeEdit';
+// Author: Ling Shan Matthew Ng, Naomy Tung 
+// Version 0.2
+// Date: 7/2/2023
 
-import { fetchFromAPI } from '../../constants';
+// Description: This is the login page
+// Precondition: A login page that contains the login form and proper styling
+// Postcondition: Login page reformatting
+
+// Input: Currently no input available
+// Output: Currently no specific output
+
+import React from 'react';
+import { useState } from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import { Box, Stack, Typography } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom"
-import { useSelector, useDispatch } from 'react-redux'
-import { toast } from 'react-toastify'
-import { login, reset } from '../../features/accountFeatures/accountSlice'
-import { Footer } from '../../containers';
+
+import { toast } from 'react-toastify';
+import { login, reset } from '../../features/accountFeatures/accountSlice';
 import { Navbar } from '../../components';
+
 
 import './Reset.css';
 
-const Reset = () => {
+
+
+
+function Reset() {
     // const [username, setUsername] = useState('');
     // const [password, setPassword] = useState('');
 
@@ -47,7 +62,7 @@ const Reset = () => {
         }
 
         if (isSuccess && user) {
-            dispatch(reset())
+
             //if its a regular client it redirect to the logged home page (protected route)
             if (user.user.type === "Client") {
                 navigate('/')
@@ -81,67 +96,111 @@ const Reset = () => {
     }
 
     return (
-        <div>
+        <>
             <Navbar />
-            <Stack sx={{ flexDirection: { sx: "column", md: "row" }, background: "var(--color-lightgreen)" }}>
 
+            <Stack sx={{ flexDirection: { sx: "column", md: "row" }, background: "var(--color-lightgreen)" }}>
                 <Box p={0} sx={{ overflowY: "auto", height: "100vh", flex: 2 }}>
                     <div className="account">
                         <div className="account__table">
-                            <div className="reset__table-column1">
-                                <div className="reset__img">
+                            <div className="login__table-column1">
+                                <div className="login__img">
                                     <img src={images.login} alt="G_overlay" className="blur" />
                                 </div>
                             </div>
 
                             <div className="reset__table-column2">
-                                <h1 className='reset__header'>
-                                    Login
-                                </h1>
+                                <form onSubmit={onSubmit}>
+                                    <div className='reset__header'>
+                                        <h1>
+                                            Forget Passowrd
+                                        </h1>
 
-                                <h1 className='reset__message'>
-                                    Welcome back! Please enter your details.
-                                </h1>
-
-                                {isError ? <p className='login__error-message'>{errorMessage}</p> : null}
-
-
-                                <input
-                                    className='login__input'
-                                    type="text"
-                                    id="email"
-                                    name="email"
-                                    placeholder="Email"
-                                    value={email}
-                                    onChange={onChange}
-                                />
-
-                                <input
-                                    className='login__input'
-                                    type="password"
-                                    id="password"
-                                    name="password"
-                                    placeholder="Password"
-                                    value={password}
-                                    onChange={onChange}
-                                />
-
-                                <div className='login__lower-functions login__forget-link'>
-                                    <div className="login__link">
-                                        <Link to="/">Forgot password?</Link>
+                                        <p>Don't worry. We've got your back. </p>
                                     </div>
-                                    <br />
-                                    <button type="submit" className="login__button">Login</button>
-                                    <br />
-                                    <button type="submit" className="login__button">Register</button>
-                                </div>
+
+
+                                    {isError ? <p className='login__error-message'>{errorMessage}</p> : null}
+
+
+                                    <input
+                                        className='login__input'
+                                        type="text"
+                                        id="email"
+                                        name="email"
+                                        placeholder="Email"
+                                        value={email}
+                                        onChange={onChange}
+                                        required
+                                    />
+
+                                    <div className='login__lower-functions login__forget-link'>
+                                        <br />
+                                        <button type="submit" className="login__button">Send Conformation Email</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </Box>
             </Stack>
-            <Footer />
+
+
+            {/* <div className='login'>
+        <div className="login__wrapper">
+          <img src={images.login} alt="G_overlay" className='left' />
+          <h1 className='HeaderText left '>Turn your dreams into reality</h1>
         </div>
+        <form onSubmit={onSubmit}>
+          <div className='right login__form'>
+            <SubHeading title={"Login"} className='login__subheading' />
+
+            <p>Welcome back! Please enter your details</p>
+
+            {isError ? <p className='login__error-message'>{errorMessage}</p> : null}
+
+
+            <input
+              className='login__input'
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={onChange}
+              required
+            />
+
+            <input
+              className='login__input'
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={onChange}
+              required
+            />
+
+            <div className='login__lower-functions login__forget-link'>
+              <div className="login__link">
+                <Link to="/">Forgot password?</Link>
+              </div>
+              <br />
+              <button type="submit" className="login__button">Login</button>
+              <br />
+              <Link to='/signup'>
+                <button className="login__button">Register</button>
+              </Link>
+              <br />
+              <div className="login__link">
+                <Link to="/">Continue as guest</Link>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div> */}
+        </>
     );
 };
 
