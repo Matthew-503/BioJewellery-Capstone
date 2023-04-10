9de7ad1e59069e8c86977e83eff18df1acbdbcfc
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-04-09 21:29:35 -0600

    changes in Uploader component

--- capstone/src/components/Uploader/Uploader.jsx
+++ capstone/src/components/Uploader/Uploader.jsx
@@ -2,26 +2,27 @@ import React from 'react'
 import './Uploader.css'
 
 import { useState } from 'react';
-import { MdCloudUpload, MdDelete } from 'react-icons/md';
-import { AiOutlineCloudUpload, AiFillFileImage } from 'react-icons/ai';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
 
 function Uploader() {
     const [image, setImage] = useState(null)
     const [fileName, setFileName] = useState("No Selected file")
+    const [imageFile, setImageFile] = useState(null);
+
     return (
         <div className='upload'>
             <div>
                 <div onClick={() => document.querySelector(".upload__input").click()}>
                     <input
                         type="file"
-                        multiple="multiple"
                         accept='image/*'
                         className="upload__input"
                         hidden
                         onChange={({ target: { files } }) => {
-                            files[0] && setFileName(files[0].name)
                             if (files) {
+                                setFileName(files[0].name)
                                 setImage(URL.createObjectURL(files[0]))
+                                setImageFile(files[0])
                             }
                         }}
                     />
@@ -38,55 +39,12 @@ function Uploader() {
                         </>
                     }
 
-                    {/* <div className='upload-row'>
-                        <div className='upload__icons-upload'>
-                            <MdDelete
-                                onClick={() => {
-                                    setFileName("No Selected Files")
-                                    setImage(null)
-                                }}
-                            />
-                        </div>
-                    </div> */}
 
                     <div>
                         <h1>{fileName}</h1>
                     </div>
                 </div>
             </div>
-
-
-            {/* <form
-            >
-                <input type="file" multiple="multiple" accept='image/*' className="input-field" hidden
-                    onChange={({ target: { files } }) => {
-                        files[0] && setFileName(files[0].name)
-                        if (files) {
-                            setImage(URL.createObjectURL(files[0]))
-                        }
-                    }}
-                />
-                {image ?
-                    <img src={image} width={150} height={150} alt={fileName} />
-                    :
-                    <>
-                        <MdCloudUpload color='#1475cf' size={60} />
-                        <p>Browse Files to Upload</p>
-                    </>
-                }
-            </form>
-            <section className='uploaded-row'>
-                <AiFillFileImage color='#1475cf' />
-                <span className='upload-content'>
-                    {fileName}
-                    <MdDelete
-                        onClick={() => {
-                            setFileName("No Selected Files")
-                            setImage(null)
-                        }}
-                    />
-                </span>
-            </section> */}
 
         </div>
     )
