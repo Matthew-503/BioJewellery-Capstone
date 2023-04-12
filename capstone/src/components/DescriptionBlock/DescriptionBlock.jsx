c7fbaeb4dea694663ae2afee932823f58ec638b4
Matthew Ng <nglingshan30@gmail.com>
2023-04-12 17:50:17 -0600

    Modified Description block mobile view

--- capstone/src/components/DescriptionBlock/DescriptionBlock.jsx
+++ capstone/src/components/DescriptionBlock/DescriptionBlock.jsx
@@ -48,43 +48,80 @@ const DescriptionBlock = () => {
         //     </table>
         // </div>
 
+        <div>
+            <div className='description'>
+                <div className='description__category'>
+                    <p>
+                        Product Name:
+                    </p>
 
-        <div className='description'>
-            <div className='description__category'>
-                <p>
-                    Product Name:
-                </p>
+                    <p>
+                        Product Type:
+                    </p>
 
-                <p>
-                    Product Type:
-                </p>
+                    <p>
+                        Color:
+                    </p>
 
-                <p>
-                    Color:
-                </p>
-
-                <p>
-                    Ratings:
-                </p>
-            </div>
-
-            <div className='description__detail'>
-                <p>
-                    {selectedProduct.name}
-                </p>
-
-                <p>
-                    {selectedProduct.type}
-                </p>
-
-                <p>
-                    {selectedProduct.colour}
-                </p>
-
-                <div className="description__rating">
-                    <Rating starRating={stars} />
+                    <p>
+                        Ratings:
+                    </p>
                 </div>
 
+                <div className='description__detail'>
+                    <p>
+                        {selectedProduct.name}
+                    </p>
+
+                    <p>
+                        {selectedProduct.type}
+                    </p>
+
+                    <p>
+                        {selectedProduct.colour}
+                    </p>
+
+                    <div className="description__rating">
+                        <Rating starRating={stars} />
+                    </div>
+
+                </div>
+            </div>
+
+            <div className='description__small'>
+                <div>
+                    <p className='description__category-small'>
+                        Product Name:
+                    </p>
+
+                    <p className='description__detail-small'>
+                        {selectedProduct.name}
+                    </p>
+
+                    <p className='description__category-small'>
+                        Product Type:
+                    </p>
+
+                    <p className='description__detail-small'>
+                        {selectedProduct.type}
+                    </p>
+
+                    <p className='description__category-small'>
+                        Color:
+                    </p>
+
+                    <p className='description__detail-small'>
+                        {selectedProduct.colour}
+                    </p>
+
+                    <p className='description__category-small'>
+                        Ratings:
+                    </p>
+
+                    <div className="description__rating">
+                        <Rating starRating={stars} />
+                    </div>
+                </div>
             </div>
         </div>
     );
