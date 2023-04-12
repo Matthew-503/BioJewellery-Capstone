c07afdbd703dbffebe917c533850f9bd3d403880
Matthew Ng <nglingshan30@gmail.com>
2023-04-12 15:48:15 -0600

    Update Home.jsx

--- capstone/src/containers/Home/Home.jsx
+++ capstone/src/containers/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Header, Gallery, Follow, Benefits, ProductDetail, Login, SignUp, AboutUs, Error, Policy, Feedback, Account, AccountHistory, OrderConfirmation, Shipping, EmpAddProduct, EmployeeSettings, EmpAccount, EmpManageProduct, Return, Reset, ResetConfirm } from '../../containers';
+import { Header, Gallery, Follow, Benefits, ProductDetail, Login, SignUp, AboutUs, Error, Policy, Feedback, Account, AccountHistory, OrderConfirmation, Shipping, EmpAddProduct, EmployeeSettings, EmpAccount, EmpManageProduct, Return, Reset, ResetConfirm, Feed, ShopProduct } from '../../containers';
+import { ShopSidebar } from '../../components';
 const Home = () => {
   return (
     <div>
@@ -20,6 +21,9 @@ const Home = () => {
       {/* <EmpAdd /> */}
       {/* <EmpAccount /> */}
       {/* <EmpManageProduct /> */}
+      {/* <Feed /> */}
+      {/* <ShopProduct /> */}
+      {/* <ShopSidebar /> */}
       {/* <Shipping /> */}
       {/* <Return /> */}
       {/* <Reset /> */}
