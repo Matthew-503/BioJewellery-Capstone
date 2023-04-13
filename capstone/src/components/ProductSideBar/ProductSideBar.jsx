2f7e26a4f2e63f087fb759e2ff053c5ce6dc12da
Matthew Ng <nglingshan30@gmail.com>
2023-04-13 15:03:12 -0600

    Modified product sidebar

--- capstone/src/components/ProductSideBar/ProductSideBar.jsx
+++ capstone/src/components/ProductSideBar/ProductSideBar.jsx
@@ -27,16 +27,12 @@ const SideBar = ({ selectedCategory, setSelectedCategory }) => {
                                 setSelectedCategory(category.name)
                                 dispatch(sortProducts(category.name))
                             }}
-                            style={{
-                                background: category.name === selectedCategory && "#bcf6b1",
-                                color: "#000000",
-                            }}
                             key={category.name}
                         >
-                            <span style={{ color: category.name === selectedCategory ? "black" : "#072d2e" }} className='grid-item'>
+                            <span className='grid-item sidebar__content'>
                                 {category.icon}
                             </span>
-                            <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }} className='grid-item sidebar__content'>
+                            <span className='grid-item sidebar__content'>
                                 {category.name}
                             </span>
                         </button>
